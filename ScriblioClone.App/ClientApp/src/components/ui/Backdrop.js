289ac44067b877e28b0e0f15a068b36f255c7bb9
Filip Björnåsen <filip@mosquito.se>
2020-11-04 19:50:45 +0100

    Removed old api and client

--- ScriblioClone.App/ClientApp/src/components/ui/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './css/Backdrop.module.css';
-
-const Backdrop = props => {
-    return props.show ? <div onClick={props.onClick}></div> : null;
-};
-
-export default Backdrop;
--- ScriblioClone.App/ClientApp/src/components/ui/TitleLogo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TitleLogo = () => {
-    return (
-        <h1>Draw that thing!</h1>
-    );
-};
-
-export default TitleLogo;
