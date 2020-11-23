e83eaca836a17dc44c4dcccf86e2cde0fc8e3f6a
Filip Björnåsen <filip@mosquito.se>
2020-11-23 19:42:36 +0100

    Cleanup and renaming

--- Scribble.Client/Startup.cs
+++ Scribble.Client/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace DrawioFrontend
+namespace Scribble.Client
 {
     public class Startup
     {
