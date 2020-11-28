89f5df818f6f60d54464bd7fa657b5013b0d2d2e
Filip Björnåsen <filip@mosquito.se>
2020-11-28 13:47:40 +0100

    Timer function to delete old games every night

--- Scribble.Functions/Functions/DeleteOldGames.cs
+++ Scribble.Functions/Functions/DeleteOldGames.cs
@@ -1,19 +1,36 @@
-using System;
+using DurableTask.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Scribble.Functions.Functions
 {
     public static class DeleteOldGames
     {
         [FunctionName(nameof(DeleteOldGames))]
-        public static void Run([TimerTrigger("0 2 * * *")]TimerInfo myTimer, 
+        public static Task Run([TimerTrigger("0 2 * * *")
+
+            ]TimerInfo myTimer,
             ILogger log,
-            [DurableClient] IDurableEntityClient client)
+            [DurableClient] IDurableOrchestrationClient client,)
         {
             log.LogInformation($"C# Timer trigger function {nameof(DeleteOldGames)} executed at: {DateTime.Now}");
+            return client.PurgeInstanceHistoryAsync(
+                DateTime.MinValue,
+                DateTime.UtcNow.AddDays(-1),
+                new List<OrchestrationStatus>
+                {
+                    OrchestrationStatus.Completed,
+                    OrchestrationStatus.Failed,
+                    OrchestrationStatus.Canceled,
+                    OrchestrationStatus.ContinuedAsNew,
+                    OrchestrationStatus.Pending,
+                    OrchestrationStatus.Running,
+                    OrchestrationStatus.Terminated,
+                });
 
         }
     }
